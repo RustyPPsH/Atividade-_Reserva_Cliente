d6aa029fc380129d3786134e1fa65c5bf34447e3
RustyPPsH <rafaelsonoki@hotmail.com>
2023-04-11 19:44:45 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            ClienteSocket cliente = new ClienteSocket();
+            cliente.comunicarComServidor();
+        } catch (Exception e) {
+            System.out.println("Exceção:" + e);
+        }
+    }
+}
